60cb899097b46b15c2abad42b306d70e969aa315
oozye1 <jamesfairfoul@gmail.com>
2025-07-24 22:37:33 +0100

    I ASSUME FIXED BUT FOR HOW LONG!!!! ARGHHHHHHH

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "co.uk.doverguitarteacher.rubbishdayreminder"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
